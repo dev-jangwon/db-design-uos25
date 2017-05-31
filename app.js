6b9e2004fce31ee5c60c465b83bce190e8cb9f47
SegeonKim <segeon215@naver.com>
2017-05-31 15:38:17 +0900

    Setting

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var port = process.env.PORT || 3000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -40,7 +42,16 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error.html');
+});
+
+process.on('uncaughtException', function(err) {
+    console.log( " UNCAUGHT EXCEPTION " );
+    console.log( "[Inside 'uncaughtException' event] " + err.stack || err.message );
+});
+
+var server = app.listen(port, function(){
+ console.log("Express server has started on port", port);
 });
 
 module.exports = app;
